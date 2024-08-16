ef7d5e87c1352ae02318c39d435ce922112030b6
Bilal Saeed <bilalsaeedhb@gmail.com>
2024-08-16 21:52:53 +0500

    enabled firebase hosting with git workflow and added logic for form submission using server actions

--- src/app/actions.js
+++ src/app/actions.js
@@ -7,4 +7,15 @@ import { getFirestore } from "firebase/firestore";
 // This is a Server Action
 // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
 // Replace the function below
-export async function handleReviewFormSubmission(data) {}
+export async function handleReviewFormSubmission(data) {
+  const { app } = await getAuthenticatedAppForUser();
+  const db = getFirestore(app);
+
+  await addReviewToRestaurant(db, data.get("restaurantId"), {
+    text: data.get("text"),
+    rating: data.get("rating"),
+
+    // This came from a hidden form field.
+    userId: data.get("userId"),
+  });
+}
--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -29,16 +29,55 @@ export async function updateRestaurantImageReference(
 }
 
 const updateWithRating = async (
-	transaction,
-	docRef,
-	newRatingDocument,
-	review
+  transaction,
+  docRef,
+  newRatingDocument,
+  review
 ) => {
-	return;
+  const restaurant = await transaction.get(docRef);
+  const data = restaurant.data();
+  const newNumRatings = data?.numRatings ? data.numRatings + 1 : 1;
+  const newSumRating = (data?.sumRating || 0) + Number(review.rating);
+  const newAverage = newSumRating / newNumRatings;
+
+  transaction.update(docRef, {
+    numRatings: newNumRatings,
+    sumRating: newSumRating,
+    avgRating: newAverage,
+  });
+
+  transaction.set(newRatingDocument, {
+    ...review,
+    timestamp: Timestamp.fromDate(new Date()),
+  });
 };
 
 export async function addReviewToRestaurant(db, restaurantId, review) {
-	return;
+  if (!restaurantId) {
+    throw new Error("No restaurant ID has been provided.");
+  }
+
+  if (!review) {
+    throw new Error("A valid review has not been provided.");
+  }
+
+  try {
+    const docRef = doc(collection(db, "restaurants"), restaurantId);
+    const newRatingDocument = doc(
+      collection(db, `restaurants/${restaurantId}/ratings`)
+    );
+
+    // corrected line
+    await runTransaction(db, (transaction) =>
+      updateWithRating(transaction, docRef, newRatingDocument, review)
+    );
+  } catch (error) {
+    console.error(
+      "There was an error adding the rating to the restaurant",
+      error
+    );
+    throw error;
+  }
 }
 
 function applyQueryFilters(q, { category, city, price, sort }) {
