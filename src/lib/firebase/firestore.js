e5fea13540cac71da56c7cea32b6fbd64fcbf571
Bilal Saeed <bilalsaeedhb@gmail.com>
2024-08-16 12:01:11 +0500

    Read the list of restaurants from Firestore

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -42,11 +42,36 @@ export async function addReviewToRestaurant(db, restaurantId, review) {
 }
 
 function applyQueryFilters(q, { category, city, price, sort }) {
-	return;
+  if (category) {
+    q = query(q, where("category", "==", category));
+  }
+  if (city) {
+    q = query(q, where("city", "==", city));
+  }
+  if (price) {
+    q = query(q, where("price", "==", price.length));
+  }
+  if (sort === "Rating" || !sort) {
+    q = query(q, orderBy("avgRating", "desc"));
+  } else if (sort === "Review") {
+    q = query(q, orderBy("numRatings", "desc"));
+  }
+  return q;
 }
 
 export async function getRestaurants(db = db, filters = {}) {
-	return [];
+  let q = query(collection(db, "restaurants"));
+
+  q = applyQueryFilters(q, filters);
+  const results = await getDocs(q);
+  return results.docs.map((doc) => {
+    return {
+      id: doc.id,
+      ...doc.data(),
+      // Only plain objects can be passed to Client Components from Server Components
+      timestamp: doc.data().timestamp.toDate(),
+    };
+  });
 }
 
 export function getRestaurantsSnapshot(cb, filters = {}) {
