af3aa9a822d8f28cd881f1ce20d8b8c204c22d7e
Bilal Saeed <bilalsaeedhb@gmail.com>
2024-08-16 21:55:08 +0500

    Ability to change restaurant photo

--- src/lib/firebase/storage.js
+++ src/lib/firebase/storage.js
@@ -5,7 +5,27 @@ import { storage } from "@/src/lib/firebase/clientApp";
 import { updateRestaurantImageReference } from "@/src/lib/firebase/firestore";
 
 // Replace the two functions below
-export async function updateRestaurantImage(restaurantId, image) {}
+export async function updateRestaurantImage(restaurantId, image) {
+  try {
+    if (!restaurantId) throw new Error("No restaurant ID has been provided.");
 
-async function uploadImage(restaurantId, image) {}
+    if (!image || !image.name)
+      throw new Error("A valid image has not been provided.");
+
+    const publicImageUrl = await uploadImage(restaurantId, image);
+    await updateRestaurantImageReference(restaurantId, publicImageUrl);
+
+    return publicImageUrl;
+  } catch (error) {
+    console.error("Error processing request:", error);
+  }
+}
+
+async function uploadImage(restaurantId, image) {
+  const filePath = `images/${restaurantId}/${image.name}`;
+  const newImageRef = ref(storage, filePath);
+  await uploadBytesResumable(newImageRef, image);
+
+  return await getDownloadURL(newImageRef);
+}
 // Replace the two functions above
